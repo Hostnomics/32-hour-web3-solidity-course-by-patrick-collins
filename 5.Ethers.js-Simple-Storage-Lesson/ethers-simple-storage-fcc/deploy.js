6d327a1ec505e8f054405bc3bcd56601b50f55f3
hostnomics <hostnomics@gmail.com>
2023-03-10 22:02:58 -0500

    Completed Lesson 5. Deployed Ethers.js simple storage contract to Alchemey

--- 5.Ethers.js-Simple-Storage-Lesson/ethers-simple-storage-fcc/deploy.js
+++ 5.Ethers.js-Simple-Storage-Lesson/ethers-simple-storage-fcc/deploy.js
@@ -16,10 +16,13 @@
     // const provider = new ethers.JsonRpcProvider('http://127.0.0.1:7545');        
     //Switched back to ethers v 5.6.2:
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);                  
-    const wallet = new ethers.Wallet(
-      process.env.PRIVATE_KEY, 
-      provider
-    ); 
+    
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider); 
+
+//simple encryption
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8"); 
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
+    // wallet = await wallet.connect(provider);
     
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8"); 
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
@@ -30,12 +33,16 @@
   const contract = await contractFactory.deploy(); 
   // const transactionReceipt = await contract.deployTransaction.wait(1); 
   await contract.deployTransaction.wait(1);
+
+
+  //(8:02:55) - Add to test our connection to Alchemy
+  console.log(`Contract Address: ${contract.address}`);
     
     // console.log("Let's deploy with only transaction data!");
 
     const currentFavoriteNumber = await contract.retrieve(); 
-    // console.log(currentFavoriteNumber);
-    // console.log(currentFavoriteNumber.toString());
+          // console.log(currentFavoriteNumber);
+          // console.log(currentFavoriteNumber.toString());
 
     console.log(`Current Favorite Number is: ${currentFavoriteNumber.toString()}`);
 
